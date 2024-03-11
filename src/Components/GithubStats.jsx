8ea8a1965cd62fa4413132cc6c11cee6cc0d2228
yelmouss <yelmouss.devt@gmail.com>
2024-03-11 15:58:56 +0000

    config pie

--- src/Components/GithubStats.jsx
+++ src/Components/GithubStats.jsx
@@ -1,8 +1,9 @@
 import React, { useEffect, useState } from 'react';
 import { VictoryPie } from 'victory';
-
+import { VictoryTooltip } from "victory-tooltip";
 const LanguagesStats = () => {
     const [languageStats, setLanguageStats] = useState([]);
+    const [label, setLabel] = useState(false);
     useEffect(() => {
         fetch('https://api.github.com/user/repos', {
             headers: {
@@ -44,24 +45,30 @@ const LanguagesStats = () => {
                 <VictoryPie
                     colorScale={["#ff6384", "#36a2eb", "#ffce56", "#4bc0c0", "#9966ff", "#ff9f40", "#80ff80", "#ff6666", "#6699ff", "#ff9999"]}
                     data={languageStats}
-                    animate={{
-                        duration: 2000
-                    }}
+                    // animate={{
+                    //     duration: 2000
+                    // }}
                     cornerRadius={2}
-                    innerRadius={60}
+                    innerRadius={(val) => {
+                        return val.datum.x === "firefox" ? 170 : 170;
+                      }}
                     labelRadius={80}
                     labelPlacement="parallel" // Place les étiquettes à l'extérieur et les relie avec des lignes
-                    labels={({ datum }) => `${datum.x}: \n ${(datum.y / languageStats.reduce((acc, curr) => acc + curr.y, 0) * 100).toFixed(2)}%`}
+                    labels={({ datum }) => `${datum.x} : \n ${(datum.y / languageStats.reduce((acc, curr) => acc + curr.y, 0) * 100).toFixed(2)}%`}
                     style={{
                         labels: {
-                            fill: '#000',
+                            fill: 'white',
                             fontWeight: 'bold',
                         },
                         parent: {
                             userSelect: 'none' // Pour empêcher la sélection accidentelle des étiquettes
                         }
                     }}
-                    padAngle={2}
+                    tooltip={"dshv"}
+                    padAngle={({ datum }) => 2}
+                    labelComponent={
+                        label ? <VictoryTooltip dy={0} centerOffset={{ x: 25 }} /> : undefined
+                      }
                 />
 
             </div>
