4d485a2a51c26fd440285edbf6e85e57e7bf3bd8
yelmouss <yelmouss.devt@gmail.com>
2024-03-11 15:49:06 +0000

    Graph done

--- src/Components/GithubStats.jsx
+++ src/Components/GithubStats.jsx
@@ -1,11 +1,10 @@
-
 import React, { useEffect, useState } from 'react';
 import { VictoryPie } from 'victory';
 
 const LanguagesStats = () => {
     const [languageStats, setLanguageStats] = useState([]);
     useEffect(() => {
-         fetch('https://api.github.com/user/repos', {
+        fetch('https://api.github.com/user/repos', {
             headers: {
                 Authorization: `Bearer ${process.env.REACT_APP_API_URL}`,
             },
@@ -39,27 +38,30 @@ const LanguagesStats = () => {
 
     return (
         <div className='container d-flex flex-column align-items-center'>
-            <h5 className='align-self-start'>Programming Languages Statistics</h5>
+            <h5 className='align-self-start'>Programming Languages and Frameworks Statistics from GIT repositories</h5>
             <div style={{ maxWidth: '100%' }} className='col-lg-7'>
 
                 <VictoryPie
                     colorScale={["#ff6384", "#36a2eb", "#ffce56", "#4bc0c0", "#9966ff", "#ff9f40", "#80ff80", "#ff6666", "#6699ff", "#ff9999"]}
                     data={languageStats}
-                    // animate={{
-                    //     duration: 2000
-                    // }}
+                    animate={{
+                        duration: 2000
+                    }}
                     cornerRadius={2}
                     innerRadius={60}
-                    labels={({ datum }) => `${datum.x}: \n ${(datum.y / languageStats.reduce((acc, curr) => acc + curr.y, 0) * 100).toFixed(2)}%`}
                     labelRadius={80}
+                    labelPlacement="parallel" // Place les étiquettes à l'extérieur et les relie avec des lignes
+                    labels={({ datum }) => `${datum.x}: \n ${(datum.y / languageStats.reduce((acc, curr) => acc + curr.y, 0) * 100).toFixed(2)}%`}
                     style={{
                         labels: {
                             fill: '#000',
                             fontWeight: 'bold',
-                           padding: '5px', // Augmentez ou diminuez cette valeur selon vos besoins
-                            // textAnchor: 'middle',
+                        },
+                        parent: {
+                            userSelect: 'none' // Pour empêcher la sélection accidentelle des étiquettes
                         }
                     }}
+                    padAngle={2}
                 />
 
             </div>
